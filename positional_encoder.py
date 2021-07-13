61554489aa8141582a235a60ae6117b1a7db6d62
Siyuan Peng <siyuan.kevin.peng@gmail.com>
2021-07-13 13:44:57 -0400

    finish tensorflow version of positional encoder

--- positional_encoder.py
+++ positional_encoder.py
@@ -34,15 +34,28 @@ def get_2d_pos_enc(height, width, n_channels):
     :param width: width of the image
     :return: (height, width, n_channels) position encoding matrix
     """
-    pe = np.empty(shape=(height, width, n_channels))
+    pe = np.empty(shape=(n_channels, height, width))
 
     d_model = int(n_channels / 2)
     div_term = np.exp(np.arange(0., d_model, 2) * -(math.log(10000.0) / d_model)) # (n_channels/2)
     pos_w = np.expand_dims(np.arange(0., width), axis=1) 
     pos_h = np.expand_dims(np.arange(0., height), axis=1)
 
-    pe[0:d_model:2, :, :] = np.sin(pos_w * div_term)
+    pe[0:d_model:2, :, :] = np.expand_dims(np.repeat(np.sin(pos_w * div_term).T, height, axis=0), axis=0)
+    pe[1:d_model:2, :, :] = np.expand_dims(np.repeat(np.cos(pos_w * div_term).T, height, axis=0), axis=0)
+    pe[d_model::2, :, :] = np.expand_dims(np.repeat(np.sin(pos_h * div_term), width, axis=1), axis=0)
+    pe[d_model + 1::2, :, :] = np.expand_dims(np.repeat(np.cos(pos_h * div_term), width, axis=1), axis=0)
+    return pe
 
 
 
+if __name__=="__main__":
+    d_model = 4
+    height = 3
+    width = 5
 
+    print(f'positionalencoding2d shape: {positionalencoding2d(d_model, height, width).shape}')
+    print(f'positionalencoding2d: {positionalencoding2d(d_model, height, width)}')
+    print('----')
+    print(f'get_2d_pos_enc shape:{get_2d_pos_enc(height, width, d_model).shape}')
+    print(f'get_2d_pos_enc:{get_2d_pos_enc(height, width, d_model)}')
